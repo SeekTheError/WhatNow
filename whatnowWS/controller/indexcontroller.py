a6874f04617ca01348a5d232ac60b89334d7aa06
SeekTheError <remi.bouchar@gmail.com>
2011-05-23 11:52:53 +0900

    c

--- whatnowWS/controller/indexcontroller.py
+++ whatnowWS/controller/indexcontroller.py
@@ -1,10 +1,18 @@
 #Author Remi Bouchar
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
-from security.userauth import checkSession
+from security.userauth import checkSession, getCurrentUser
 
 def view(request) :
   t = loader.get_template('index.html')
   context=RequestContext(request)
   context=checkSession(request,context)
   return HttpResponse(t.render(context))
+  
+def addKeyword(request):
+  context=checkSession(request,context)
+  user = getCurrentUser(context)
+  user = user.findById()
+  fav=request.GET['favorite']
+  user.topics.append(fav);
+  
